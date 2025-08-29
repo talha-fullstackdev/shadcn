0f335fe3313c9d2d45077e13618413d4b1656412
talha-fullstackdev <talhafullstack786@gmail.com>
2025-08-29 12:39:55 +0500

    add dynamic date and time in toast message

--- src/app/shadcn/toast/page.js
+++ src/app/shadcn/toast/page.js
@@ -1,15 +1,37 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { toast } from 'sonner'
+"use client";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { toast } from "sonner";
 
 const ToastPage = () => {
-  return (
-    <div >
-        <Button onClick={()=>toast.error("hello next js")}>Toast</Button> 
-    </div>
-  )
-}
+  const handleToast = () => {
+    const now = new Date();
+    const weekday = now.toLocaleDateString(undefined, { weekday: "long" });
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+    toast.success("Hello Next.js ", {
+      description: `${weekday}, ${date} — ${time}`,
+      action: {
+        label: "X",
+        onClick: () => {
+          console.log("undo");
+        },
+      },
+    });
+  };
 
-export default ToastPage
+  return (
+    <div>
+      <Button onClick={handleToast}>Toast</Button>
+    </div>
+  );
+};
+
+export default ToastPage;
+
 // we can also add toast colors like success , error , warning etc
+// here we can pass more description to toast like that like description and action which contain a call back function
